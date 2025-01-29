806ecfb273bd86ef3d753e38b2c7205e5d83ebe5
nitin <nitingoley21@gmail.com>
2025-01-29 22:38:37 +0530

    updates vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 import path from 'path';
 
- 
+// Fix for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
